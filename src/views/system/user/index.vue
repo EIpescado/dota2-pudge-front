28878ecaafe585af96dd70b5aed1f3d1c1e278a0
yuqian <peer_number>
2020-10-08 22:20:28 +0800

    通用样式调整

---
+++ src/views/system/user/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="app-container">
+
+    <!--查询-->
+    <div class="filter-container">
+      <el-form ref="qo" :inline="true" :model="qo" size="small">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model.trim="qo.username" @keyup.enter.native="search" />
+        </el-form-item>
+        <div class="filter-button-container">
+          <el-button type="primary" size="small" icon="el-icon-search" round @click="search">
+            搜索
+          </el-button>
+          <el-button size="small" icon="el-icon-refresh" round @click="refreshQo">
+            重置
+          </el-button>
+        </div>
+      </el-form>
+    </div>
+
+    <!--顶部按钮-->
+    <TopButton />
+
+    <!--列表-->
+    <el-table v-loading="showLoading" :data="data" size="small" highlight-current-row class="table-container">
+      <el-table-column label="用户名" prop="username" />
+      <el-table-column label="昵称" prop="nickName" />
+      <el-table-column label="手机" prop="phone" />
+      <el-table-column label="注册日期" prop="dateCreated" />
+    </el-table>
+
+    <!--分页-->
+    <pagination v-show="total>0" :total="total" :page.sync="qo.page" :size.sync="qo.size" @pagination="getData" />
+
+  </div>
+</template>
+
+<script>
+import { list } from '@/api/system/user'
+import Pagination from '@/components/Pagination'
+import TopButton from '@/components/TopButton'
+
+export default {
+  name: 'User',
+  components: { Pagination, TopButton },
+  data() {
+    return {
+      data: null, total: 0, showLoading: false,
+      qo: { page: 1, size: 10, username: '' }
+    }
+  },
+  created() {
+    this.getData()
+  },
+  methods: {
+    getData() {
+      list(this.qo).then(res => {
+        this.data = res.rows
+        this.total = res.total
+      })
+    },
+    search() {
+      this.page = 1
+      this.getData()
+    },
+    refreshQo() {
+      this.$refs.qo.resetFields()
+    }
+  }
+}
+</script>
