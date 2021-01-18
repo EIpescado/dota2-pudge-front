7661daf80050f4c301dbe0736511aad80861a198
yuqian <1724537432@qq.com>
2021-01-18 23:43:56 +0800

    文件通用组件

---
+++ src/components/SystemFileWindow/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <el-dialog :visible.sync="dialog" :title="title" append-to-body :close-on-click-modal="false" @close="cancel">
+    <el-table :data="fileList">
+      <el-table-column prop="fileName" label="文件名称">
+        <template slot-scope="{row}">
+          <el-link type="primary" @click="downLoadSingleFile(row.id)">{{ row.fileName }}</el-link>
+        </template>
+      </el-table-column>
+      <el-table-column label="文件用途">
+        <template slot-scope="{row}">
+          <el-tag type="success" effect="light">{{ formatFileTag(row.fileTag) }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="uploadPerson" label="上传人" />
+      <el-table-column prop="uploadDate" label="上传时间" />
+    </el-table>
+    <span slot="footer" class="dialog-footer">
+      <el-button v-show="fileList && fileList.length > 0 && zip" type="primary" @click="batchDownLoadInZip">批量下载</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+import { getEntityFiles, download, downloadZip } from '@/api/system/file'
+import { getIdArray, transferValueForArray, getDictSelectData } from '@/utils/common'
+export default {
+  name: 'SystemFileWindow',
+  props: {
+    zip: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      fileList: [], dialog: false, title: '附件'
+    }
+  },
+  created() {
+    getDictSelectData('system_file_tag')
+    this.showFileList('1317296928130662402')
+  },
+  methods: {
+    showFileList(entityId, title) {
+      if (title) {
+        this.title = title
+      }
+      this.dialog = true
+      getEntityFiles(entityId).then(res => { this.fileList = res })
+    },
+    cancel() {
+      this.fileList = []
+      this.dialog = false
+    },
+    downLoadSingleFile(fileId) {
+      download(fileId)
+    },
+    batchDownLoadInZip() {
+      const ids = getIdArray(this.fileList)
+      if (ids && ids.length > 0) {
+        downloadZip(ids)
+      }
+    },
+    formatFileTag(fileTag) {
+      return transferValueForArray('system_file_tag', fileTag)
+    }
+  }
+}
+</script>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -4,15 +4,19 @@
       <el-col :span="4">
         <FileUpload :file-tag="1" />
       </el-col>
+      <el-col :span="4">
+        <SystemFileWindow :zip="true" />
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
 import FileUpload from '@/components/FileUpload'
+import SystemFileWindow from '@/components/SystemFileWindow'
 export default {
   name: 'Dashboard',
-  components: { FileUpload },
+  components: { FileUpload, SystemFileWindow },
   data() {
     return {}
   },
